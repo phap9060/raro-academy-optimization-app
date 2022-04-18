2b68da424ac4a25f83e59821290ab9f5032af005
phap9060 <phap9060@gmail.com>
2022-04-18 12:02:33 -0300

    adicionando rota de color responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import Chat from "./pages/Chat";
+import { ColorResponsive } from "./pages/ColorResponsive";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 export default function App() {
   return (
-    <Chat />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Chat />} />
+        <Route path="/color" element={<ColorResponsive/>} />
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
